74359568346bd1120d78a1d523c9cb11cf0bb785
Andrew Gilbert <andrewg800@gmail.com>
2018-04-13 00:06:27 -0700

    Add support for handling ACM exports

--- gui.py
+++ gui.py
@@ -1,20 +1,56 @@
 import csv
 import pandas as pd
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, make_response
 app = Flask(__name__)
 
-data = pd.read_csv("IEEE-results--export2018.04.11-15.30.32.csv")
+data = {}
+data['ieee'] = pd.read_csv("IEEE-results--export2018.04.11-15.30.32.csv")
+acm_data = pd.read_csv("ACMDL201804116245225.csv")
+acm_data['url'] = acm_data['id'].apply("https://dl.acm.org/citation.cfm?id={}&preflayout=flat#abstract".format)
+data['acm'] = acm_data
 
-csvfile = open("output.csv", "a")
-output = csv.writer(csvfile)
-output.writerow(['paper_id', 'action'])
+
+output_files = {
+    'acm': 'output-acm.csv',
+    'ieee': 'output.csv'
+}
+
+def get_output(key):
+    outputfile = output_files[key]
+    csvfile = open(outputfile, "a")
+    output = csv.writer(csvfile)
+    return csvfile, output
+
+def get_template(key):
+    if key == 'ieee':
+        return 'paper.html'
+    elif key == 'acm':
+        return 'acm.html'
+
+def get_library():
+    return request.cookies.get('library', 'ieee')
+
+
+@app.route("/library/<libname>")
+def set_library(libname):
+    if libname == 'ieee':
+        resp = make_response("Library set to IEEE")
+        resp.set_cookie('library', 'ieee')
+    elif libname == 'acm':
+        resp = make_response("Library set to ACM")
+        resp.set_cookie('library', 'acm')
+    return resp
 
 @app.route('/<int:paper_id>')
 def show_paper(paper_id):
-    res = data.iloc[paper_id]
-    return render_template("paper.html", paper_id=paper_id, data=res)
+    key = get_library()
+    template = get_template(key)
+    print(template)
+    res = data[key].iloc[paper_id]
+    return render_template(template, paper_id=paper_id, data=res)
 
 def process_choice(action, paper_id):
+    csvfile, output = get_output(get_library())
     print(action + "ed " + paper_id)
     output.writerow([paper_id, action])
     csvfile.flush()
