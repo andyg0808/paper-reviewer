93323529d2b24cf42be59ed65101c06192b954d5
Andrew Gilbert <andrewg800@gmail.com>
2018-04-25 21:13:31 -0700

    More work on sheets tool and template

--- sheets_tool.py
+++ sheets_tool.py
@@ -30,27 +30,57 @@ def save_api_key():
     return mgr.api_callback('/')
 
 
-@app.route("/list/<int:rowid>")
-def list_sheets(rowid):
+def get_available_values(typeid):
     sheets = mgr.get_api()
     sheetid = config.get('sheetid')
-    cellrange = config.get('range')
+    cellrange = config.get('type_filters')[typeid]
+    result = sheets.spreadsheets().values().get(
+            spreadsheetId=sheetid,
+            range=cellrange,
+            majorDimension='COLUMNS'
+            ).execute()
+    return result.get('values', [])[0]
+
+
+def get_row_value(rowid):
+    sheets = mgr.get_api()
+    sheetid = config.get('sheetid')
     sheet = config.get('sheet')
     cellrange = "{}!A{}:U{}".format(sheet, rowid, rowid)
     result = sheets.spreadsheets().values().get(
              spreadsheetId=sheetid,
              range=cellrange).execute()
-    values = result.get('values', [])[0]
+    return result.get('values', [])[0]
+
+
+@app.route("/list/<int:rowid>")
+def list_sheets(rowid):
+    values = get_row_value(rowid)
+
+    paper_types = get_available_values('paper_types')
+    res_types = get_available_values('research_types')
+    tools = get_available_values('tools')
+    models = get_available_values('models')
+    measurements = get_available_values('measurements')
 
     return flask.render_template("spreadsheet.html", 
+            rowid=rowid,
             title=values[0],
-            url=values[1])
+            url=values[1],
+            paper_types=paper_types,
+            res_types=res_types,
+            tools=tools,
+            models=models,
+            measurements=measurements)
 
 
-@app.route("/set_values")
+@app.route("/set_values", methods=['POST'])
 def set_sheet_values():
+    rowid = int(flask.request.form['rowid'])
     sheets = mgr.get_api()
-    return 'abc'
+    values = get_row_value(rowid)
+    print(values)
+    return flask.redirect(flask.url_for('list_sheets', rowid=rowid+1))
 
 
 app.run('localhost', 5000)
